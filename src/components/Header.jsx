ecd96e99c038f9bab8ece0dd362c48706aab010e
Ronddaa <tronddaa@gmail.com>
2024-01-28 10:22:39 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <img src={mainLogo} alt="Logo" />
         <div className="wrapperHeaderNavigation">
           <a href="tel:+380680333203" className="phoneNumberLinkHeader">+380 68 033 32 03</a>
-          <a href="#" className="mainLinkToInstagram"><svg className='mainLinkToInstagramSVG' width={50} height={50}><use xlinkHref={`${sprite}#icon-instagram`} /></svg></a>
+          <a href="https://www.instagram.com/ivanna_katrych?igsh=NTd6cjIzejlqNmF0" className="mainLinkToInstagram" target='_blank' rel='noreferrer'><svg className='mainLinkToInstagramSVG' width={50} height={50}><use xlinkHref={`${sprite}#icon-instagram`} /></svg></a>
           <svg className='burgerMenu' width={50} height={50}><use xlinkHref={`${sprite}#icon-burger-menu`} /></svg>
         </div>
       </div>
