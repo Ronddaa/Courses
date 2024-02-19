8b1f86f10693245347ea652cdfee5ca21c455d5a
Ronddaa <tronddaa@gmail.com>
2024-02-19 16:32:09 +0100

    asd

--- src/components/InstructorOnlineLeassons.jsx
+++ src/components/InstructorOnlineLeassons.jsx
@@ -8,7 +8,7 @@ export default function InstructorOnlineLeasson() {
             <section className="instructorOnlineLeasson">
                 <div className="container wrapperinstructorOnlineLeasson">
                     <h2 className="titleInstructorOnlineLeassonF">УРОК №1: знайомство зі мною</h2>
-                    <iframe className="leassonFrst" width={1158} height={554} src="https://www.youtube.com/embed/HJA92LQrsvw?si=I8ADAgNETbHo-8n6" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe className="leassonFrst" width={1158} height={554} src="https://www.youtube.com/embed/Hc7Ctjbi3_8?si=CNbeCph_vVH8JqPO" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     <h3 className="titleSentHW">Надіслати домашнє завдання</h3>
                     <div className="wrapperHWmade">
                         <textarea name="HomeWork" id="homeWork" cols="30" rows="10"></textarea>
