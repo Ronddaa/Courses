468c01f8d1f31eaf3b5585e1b344e6e90445eded
Ronddaa <tronddaa@gmail.com>
2024-02-27 19:43:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,30 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '../testReact',
+
+  // Встановлюємо шлях до вихідної папки для збудованих файлів
+  outDir: '../dist',
+
+  // Налаштовуємо базовий URL для вашого додатку
+  base: '/',
+
+  // Налаштовуємо режим розробки або продакшену
+  mode: 'development', // або 'production'
+
+  // Встановлюємо ряд параметрів для сервера розробки
+  server: {
+    port: 3000,
+    open: true, // автоматично відкривати браузер
+    proxy: {
+      // Приклад налаштування проксі для пересилання запитів на інший сервер
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
 })
+
