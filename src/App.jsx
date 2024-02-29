7e4033b3361e05c4e8bdd7427e750358e15b0f45
Ronddaa <tronddaa@gmail.com>
2024-02-29 12:23:08 +0100

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ function Main() {
         <div className="JustWrapper">
             <header className="header">
                 <div className="container wrapperHeader">
-                    <img src={mainLogo} alt="Logo" />
+                    <img className='headerLogoMain' src={mainLogo} alt="Logo" />
                     <div className="wrapperHeaderNavigation">
                         <a href="tel:+380680333203" className="phoneNumberLinkHeader">+380 68 033 32 03</a>
                         <a href="https://www.instagram.com/ivanna_katrych?igsh=NTd6cjIzejlqNmF0" className="mainLinkToInstagram" target='_blank' rel='noreferrer'><svg className='mainLinkToInstagramSVG' width={50} height={50}><use xlinkHref={`${sprite}#icon-instagram`} /></svg></a>
