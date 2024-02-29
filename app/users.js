c4675cd41a3c82e5449196c1c8e56cc58c2b4219
Ronddaa <tronddaa@gmail.com>
2024-02-29 14:02:57 +0100

    asd

--- app/users.js
+++ app/users.js
@@ -1,10 +1,39 @@
 export default {
     "matador" : {
         "name" : 'matador',
-        "pass" : '123321'
+        "pass" : '123321',
     },
     "jsdenton" :  {
         "name" : "jsdenton",
-        "pass" : "bcheats"
-    }
+        "pass" : "bcheats",
+    },
+    "Марія Процюк": {
+        "name" : "Марія Процюк",
+        "pass" : "Gf62npUYEk93d2K8",
+    },
+    "Люда Михайловська": {
+        "name" : "Люда Михайловська",
+        "pass" : "iTJGk59c2Ab96xE2",
+    },
+    "Тетяна Гаврилюк": {
+        "name" : "Тетяна Гаврилюк",
+        "pass" : "npZ9Zh5rC4S2mL68",
+    },
+    "Валя Пилипенко": {
+        "name" : "Валя Пилипенко",
+        "pass" : "5M954nyVEb9E7Sah",
+    },
+    "Людмила Хімчинська": {
+        "name" : "Людмила Хімчинська",
+        "pass" : "37SG3C9vbtXzdT62",
+    },
+    "Боцвінок Ілона": {
+        "name" : "Боцвінок Ілона",
+        "pass" : "7j64NBsDFd83s3iH",
+    },
+    "Aneta Brudek": {
+        "name" : "Aneta Brudek",
+        "pass" : "4vB5UBkgTm3v55Z3",
+    },
+    
 };
